3da522991d7e6e7e86ff1f76f18fecd329276946
Erik Lax <erik@halon.se>
2018-11-12 13:02:05 +0100

    Include source in error

--- src/validate.ts
+++ src/validate.ts
@@ -16,8 +16,8 @@ export const validate = (config: any) =>
     var ajv = new AJV();
 
     var smtpd = ajv.compile(JSON.parse(fs.readFileSync(path.join(...findJSONSchemasRoot(), "smtpd-app.schema.json")).toString()));
-    if (!smtpd(config.smtpd)) throw smtpd.errors;
+    if (!smtpd(config.smtpd)) throw { source: 'smtpd-app', errors: smtpd.errors };
 
     var queued = ajv.compile(JSON.parse(fs.readFileSync(path.join(...findJSONSchemasRoot(), "queued-app.schema.json")).toString()));
-    if (!queued(config.queued)) throw queued.errors;
+    if (!queued(config.queued)) throw { source: 'queued-app', errors: queued.errors };
 }
