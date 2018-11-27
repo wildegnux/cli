b259a8c8e0cd97cd155f213077da6d81805c41c2
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2018-11-27 15:44:11 +0100

    Refactor ssh connector class

--- src/factory.ts
+++ src/factory.ts
@@ -30,16 +30,38 @@ export const ConnectorFactory = (settings: any) =>
 export class SSH2Connector implements IConnector
 {
     settings: any;
+    connected: boolean;
     conn?: Client;
+
 	constructor(settings: any)
 	{
         this.settings = settings;
+        this.connected = false;
+    }
+    async connect(settings: any)
+    {
+        return new Promise<Client>((resolve, reject) => {
+            var conn = new Client();
+            conn.on('error', (err) => {
+                conn.end();
+                this.connected = false;
+                reject(err);
+            });
+            conn.on('close', () => {
+                this.connected = false;
+            });
+            conn.on('ready', () => {
+                this.connected = true;
+                resolve(conn);
+            });
+            conn.connect(settings);
+        });
     }
     async openServerChannel(path: string, callback: (stream: NodeJS.ReadWriteStream) => void)
     {
 		return new Promise<any>(async (resolve, reject) => {
-            if (!this.conn)
-                this.conn = await connect(this.settings);
+            if (!this.conn || !this.connected)
+                this.conn = await this.connect(this.settings);
             this.conn.openssh_forwardInStreamLocal(path, (err) => {
                 reject(err);
             });
@@ -51,31 +73,44 @@ export class SSH2Connector implements IConnector
     }
     closeServerChannel(server: any)
     {
-        if (this.conn)
+        if (this.conn && this.connected)
             this.conn.openssh_unforwardInStreamLocal(server);
     }
 	async openChannel(path: string)
 	{
-        if (!this.conn)
-            this.conn = await connect(this.settings);
-		return await openChannel(this.conn, path);
+        if (!this.conn || !this.connected)
+            this.conn = await this.connect(this.settings);
+		return new Promise<ClientChannel>((resolve, reject) => {
+            if (!this.conn) reject('No open connection');
+            else this.conn.openssh_forwardOutStreamLocal(path, (err, stream) => {
+                if (err) reject(err);
+                resolve(stream);
+            });
+        });
     }
     async exec(program: string, argv: string[])
     {
-        if (!this.conn)
-            this.conn = await connect(this.settings);
-        return await exec(this.conn, program + " " + argv.join(" "));
+        if (!this.conn || !this.connected)
+            this.conn = await this.connect(this.settings);
+        return new Promise<ExecProgram>((resolve, reject) => {
+            if (!this.conn) reject('No open connection');
+            else this.conn.exec(program + " " + argv.join(" "), (err, stream) => {
+                if (err) reject(err);
+                resolve(stream);
+            });
+        });
     }
     dispose()
     {
-        if (this.conn)
+        if (this.conn && this.connected) {
             this.conn.end();
+        }
     }
 }
 
 export class UNIXConnector implements IConnector
 {
-	async openChannel(path: string)
+	openChannel(path: string)
 	{
 		return new Promise<NodeJS.ReadWriteStream>((resolve, reject) => {
 			const client = net.createConnection({ path: path }, async () => {
@@ -83,7 +118,7 @@ export class UNIXConnector implements IConnector
 			});
 		});
     }
-    async openServerChannel(path: string, callback: (stream: NodeJS.ReadWriteStream) => void)
+    openServerChannel(path: string, callback: (stream: NodeJS.ReadWriteStream) => void)
     {
         return new Promise<any>(async (resolve, reject) => {
             var s = net.createServer((client) => {
@@ -97,7 +132,7 @@ export class UNIXConnector implements IConnector
     {
         server.close();
     }
-	async exec(program: string, argv: string[])
+	exec(program: string, argv: string[])
 	{
 		return new Promise<ExecProgram>((resolve, reject) => {
 			resolve(child_process.spawn(program, argv));
@@ -106,42 +141,4 @@ export class UNIXConnector implements IConnector
 	dispose()
 	{
 	}
-}
-
-export const openChannel = async(client: Client, channel: string) =>
-{
-	return new Promise<ClientChannel>((resolve, reject) => {
-		client.openssh_forwardOutStreamLocal(channel, (err, stream) => {
-			if (err) {
-                client.end();
-                reject(err);
-            }
-			resolve(stream);
-		});
-	});
-}
-
-const connect = async (settings: any) =>
-{
-	return new Promise<Client>((resolve, reject) => {
-		var conn = new Client();
-		conn.on('error', (err) => {
-			conn.end();
-			reject(err);
-		});
-		conn.on('ready', () => {
-			resolve(conn);
-        });
-		conn.connect(settings);
-	});
-}
-
-export const exec = async (client: Client, program: string) =>
-{
-	return new Promise<ExecProgram>((resolve, reject) => {
-		client.exec(program, (err, stream) => {
-            if (err) reject(err);
-            resolve(stream);
-		});
-	});
 }
